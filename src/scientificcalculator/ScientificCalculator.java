1bb5824bb34f6a8f9f5e1c2cec8aaea91ff48096
setsunafjustin00208 <setsunafjustin00208@outlook.com>
2022-07-26 10:01:40 +0800

    Fianl Commit on 07-08-21

---
+++ src/scientificcalculator/ScientificCalculator.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scientificcalculator;
+
+/**
+ *
+ * @author Setsuna F. Seiei
+ */
+public class ScientificCalculator {
+    public static void main(String[] args){
+        
+    }
+}
